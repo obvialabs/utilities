17b205ecbe283592ab6453c6cb236542f41576ff
selcukcukur <selcukcukur@outlook.com.tr>
2026-04-01 00:37:21 +0300

    chore(workspace): switch webgl utilities to scoped @obvia/webgl

--- src/core/functions/index.ts
+++ src/core/functions/index.ts
@@ -1,38 +1,3 @@
 // Tailwind
 export { cn } from "./tailwind/cn"
 export { cv, type VariantOf } from "./tailwind/cv"
-
-// WebGL
-export {
-  canvasContext,
-  resizeCanvas,
-  type CanvasContextOptions,
-  type ResizeCanvasOptions
-} from "./webgl/context"
-export {
-  createBuffer,
-  createQuadBuffer,
-  createQuadIndexBuffer,
-  createCubeBuffer,
-  createCubeIndexBuffer,
-  createTriangleBuffer,
-  createTriangleIndexBuffer,
-  updateBuffer,
-  updateBufferPartial,
-  deleteBuffer
-} from "./webgl/buffer"
-export {
-  bindAttribute,
-  enableAttribute,
-  disableAttribute
-} from "./webgl/attribute"
-export {
-  setUniform1f,
-  setUniform1i,
-  setUniform2f,
-  setUniform3f,
-  setUniformMatrix2fv,
-  setUniformMatrix3fv,
-  setUniformMatrix4fv,
-  setUniformSampler
-} from "./webgl/uniform"
